ab58f6337f24eb64bfc2fcb5f8d8a55f2935b590
samikshyakc1996 <samikshyakc1996@gmail.com>
2021-12-03 15:35:18 -0500

    ensures that recipes can be added ad tested

--- test/cookbook.test.js
+++ test/cookbook.test.js
@@ -1,27 +1,34 @@
-const { Cookbook } = require('../src/cookbook');
+const { Cookbook } = require("../src/cookbook");
 
-describe('Cookbook', () => {
-  describe('Adding recipes', () => {
-    test('should allow a recipe to be added', () => {
-
+describe("Cookbook", () => {
+  describe("Adding recipes", () => {
+    test("should allow a recipe to be added", () => {
+      //Arrange
+      const myCookbook = new Cookbook();
+      //Act
+      myCookbook.addRecipe("Cookie Recipe", [
+        "eggs",
+        "flour",
+        "chocolate chip",
+        "yeast",
+        "sugar",
+      ]);
+      //Assert
+      expect(myCookbook.recipes).toEqual({
+        "Cookie Recipe": ["eggs", "flour", "chocolate chip", "yeast", "sugar"],
+      });
     });
   });
 
-  describe('Listing recipes', () => {
-    test('should allow the recipes to be listed', () => {
-
-    });
+  describe("Listing recipes", () => {
+    test("should allow the recipes to be listed", () => {});
   });
 
-  describe('Retrieving a recipe', () => {
-    test('should allow the ingredients for a recipe to be retrieved', () => {
-
-    });
+  describe("Retrieving a recipe", () => {
+    test("should allow the ingredients for a recipe to be retrieved", () => {});
   });
 
-  describe('Deleting a recipe', () => {
-    test('should allow a recipe to be deleted', () => {
-
-    });
+  describe("Deleting a recipe", () => {
+    test("should allow a recipe to be deleted", () => {});
   });
 });
