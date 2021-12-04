d7a88ec0b02dae224a263e057a388dc1e7088ebd
samikshyakc1996 <samikshyakc1996@gmail.com>
2021-12-03 23:45:36 -0500

    Successfully listed recipes in CLI file

--- test/cookbook-cli.test.js
+++ test/cookbook-cli.test.js
@@ -8,6 +8,7 @@ describe("CookbookCli", () => {
       const myCookbookCli = new CookbookCli(myCookbook);
 
       const message = myCookbookCli.add("hotdog", ["meat", "bread"]);
+
       expect(message).toEqual(
         `Successfully added the following recipe: hotdog`
       );
@@ -16,6 +17,21 @@ describe("CookbookCli", () => {
 
   describe("Listing recipes", () => {
     test("should display the correct message listing all of the recipe names", () => {});
+    const myCookbook = new Cookbook();
+    const myCookbookCli = new CookbookCli(myCookbook);
+
+    myCookbookCli.add("hotdog", ["meat", "bread"]);
+    myCookbookCli.add("ChickenChilli", [
+      "Chicken",
+      "Bell Pepper",
+      "Onion",
+      "Ketchup",
+    ]);
+    const myRecipeList = myCookbookCli.list();
+    console.log(myCookbookCli.list());
+    expect(myRecipeList).toEqual(
+      `You have the following recipes: hotdog,ChickenChilli`
+    );
   });
 
   describe("Retrieving a recipe", () => {
